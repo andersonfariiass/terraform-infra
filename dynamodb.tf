bde25e8cd072b4058a29bcf0241d1ceb261dd45a
Anderson Farias <31108276+andersonfariiass@users.noreply.github.com>
2019-10-29 21:06:16 -0300

    Create dynamodb.tf

---
+++ dynamodb.tf
@@ -0,0 +1,16 @@
+resource "aws_dynamodb_table" "dynamodb-prod" {
+  name           = "GameScores"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+}
